e189133a33061adb1491f98c673686e7fe34e024
Nick Moriarty <nzmoriarty@gmail.com>
2020-06-09 18:24:43 +1200

    first commit

---
+++ precache-manifest.60f398525c4e59727218ea806eb5f9e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfb3b23ef533a32a9d63cc7e542cf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94e16a128004c2276554",
+    "url": "/static/css/main.fbd07e6b.chunk.css"
+  },
+  {
+    "revision": "f3b58ab4a38d405ff5d6",
+    "url": "/static/js/2.e4022cd4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e4022cd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e16a128004c2276554",
+    "url": "/static/js/main.14056588.chunk.js"
+  },
+  {
+    "revision": "7d499a4ae5ba60415a49",
+    "url": "/static/js/runtime-main.b2354d74.js"
+  }
+]);
